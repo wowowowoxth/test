a85d42c34a6a5bd70b6aaeb517fdb162f8372ad7
QUICKLY0000 <1873051284@qq.com>
2023-03-28 22:21:56 +0800

    first commit

---
+++ ha-start.sh
@@ -0,0 +1,31 @@
+﻿#!/bin/bash
+if [ $# -lt 1 ]
+then
+    echo "No Args Input..."
+    exit ;
+fi
+case $1 in
+"start")
+        echo " =================== 启动 hadoop集群 ==================="
+
+        echo " --------------- 启动 hdfs ---------------"
+        ssh xth001 "/opt/module/hadoop/sbin/start-dfs.sh"
+        echo " --------------- 启动 yarn ---------------"
+        ssh xth003 "/opt/module/hadoop/sbin/start-yarn.sh"
+        echo " --------------- 启动 historyserver ---------------"
+        ssh xth002 "/opt/module/hadoop/bin/mapred --daemon start historyserver"
+;;
+"stop")
+        echo " =================== 关闭 hadoop集群 ==================="
+
+        echo " --------------- 关闭 historyserver ---------------"
+        ssh xth002 "/opt/module/hadoop/bin/mapred --daemon stop historyserver"
+        echo " --------------- 关闭 yarn ---------------"
+        ssh xth003 "/opt/module/hadoop/sbin/stop-yarn.sh"
+        echo " --------------- 关闭 hdfs ---------------"
+        ssh xth002 "/opt/module/hadoop/sbin/stop-dfs.sh"
+;;
+*)
+    echo "Input Args Error..."
+;;
+esac
---
+++ kf.sh
@@ -0,0 +1,19 @@
+﻿#! /bin/bash
+
+case $1 in
+"start"){
+    for i in xth001 xth002 xth003 xth004 xth005
+    do
+        echo " --------启动 $i Kafka-------"
+        ssh $i "/opt/module/kafka/bin/kafka-server-start.sh -daemon /opt/module/kafka/config/server.properties"
+    done
+};;
+"stop"){
+    for i in xth001 xth002 xth003 xth004 xth005
+    do
+        echo " --------停止 $i Kafka-------"
+        ssh $i "/opt/module/kafka/bin/kafka-server-stop.sh "
+    done
+};;
+esac
+
